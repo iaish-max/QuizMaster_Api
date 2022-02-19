a35362bfceb8da2a848c915a063c1f762635cc73
iaish-max <aishwarybhadauriya09@gmail.com>
2022-02-20 02:48:57 +0530

    commit6

--- server2.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const app = require("./app2");
-const port = process.env.PORT || 4000;
-
-const server = http.createServer(app);
-
-server.listen(port);
--- server3.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const http = require("http");
-const app = require("./app3");
-const port = process.env.PORT || 5000;
-
-const server = http.createServer(app);
-
-server.listen(port);
